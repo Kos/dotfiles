2b85c33f7ff4596bf32c22a547413b97b2d00f9a
Tomasz Wesołowski <kosashi@gmail.com>
2018-11-27 12:04:45 +0100

    Update link.sh

--- link.sh
+++ link.sh
@@ -1,7 +1,14 @@
-#!/bin/bash -e
+#!/bin/bash
+set -e
 FILES=`find \( -type f -o -type l \) -not -path "./.git/*" -not -name link.sh`
 IFS=$'\n'
 for i in $FILES; do
-    $CMD mkdir -p ~/`dirname $i`
-    $CMD ln -v -s ~/dotfiles/$i ~/$i || true
+  $CMD mkdir -p ~/`dirname $i`
+  if [ -L ~/$i ]; then
+    true # ignore - already linked
+  elif [ -e ~/$i ]; then
+    echo "Igoring $i: (already exists as a file)"
+  else
+    $CMD ln -v -s ~/dotfiles/$i ~/$i
+  fi
 done
